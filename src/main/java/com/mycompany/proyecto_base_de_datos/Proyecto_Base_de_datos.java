f75a9b92cb19fca7f0d837e831321a8133c3169c
Jhordan <jcaballerooros@gmail.com>
2025-08-13 19:36:48 -0500

    Se añadieron los archivos iniciales

---
+++ src/main/java/com/mycompany/proyecto_base_de_datos/Proyecto_Base_de_datos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_base_de_datos;
+
+/**
+ *
+ * @author jcaba
+ */
+public class Proyecto_Base_de_datos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
