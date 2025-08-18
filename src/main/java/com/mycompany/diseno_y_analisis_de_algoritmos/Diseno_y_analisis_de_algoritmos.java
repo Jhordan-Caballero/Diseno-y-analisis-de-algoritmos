54b75d3f653455d5dbae3cd098b65ea8396ce405
Jhordan <jcaballerooros@gmail.com>
2025-08-17 21:12:26 -0500

    se creo el repositorio para el proyecto

---
+++ src/main/java/com/mycompany/diseno_y_analisis_de_algoritmos/Diseno_y_analisis_de_algoritmos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.diseno_y_analisis_de_algoritmos;
+
+/**
+ *
+ * @author jcaba
+ */
+public class Diseno_y_analisis_de_algoritmos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
